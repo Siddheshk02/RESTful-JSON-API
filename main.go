71e881059b91996f69829a64944bcb97087f1225
Siddhesh Khandagale <90148705+Siddheshk02@users.noreply.github.com>
2022-01-27 17:44:24 +0530

    changed the event with ID=1

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ type allEvents []event
 var events = allEvents{
 	{
 		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+		Title:       "Golang",
+		Description: "Go is an open source programming language used for general purpose.",
 	},
 }
 
